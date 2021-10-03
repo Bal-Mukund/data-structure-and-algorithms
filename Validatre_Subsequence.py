c9954acb13086c0bc0e3e4ea0a0fa4fc80500291
Bal Mukund <balmukundbabhanlai@gmail.com>
2021-10-03 07:18:18 +0530

    Update Validatre_Subsequence.py

--- Validatre_Subsequence.py
+++ Validatre_Subsequence.py
@@ -1,3 +1,8 @@
+""" [Queston]  Given two non-empty arrays of integers, write a function 
+that determines whether the second array is a subsequence of the first one. """
+
+
+
 def isValidSubsequence(array, sequence):
     subidx = 0
 
