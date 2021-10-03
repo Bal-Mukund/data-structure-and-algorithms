6dc1f768f069bb9450ba693f359328649fb48fcd
Bal Mukund <balmukundbabhanlai@gmail.com>
2021-10-03 11:15:46 +0530

    Update Validatre_Subsequence.py

--- Validatre_Subsequence.py
+++ Validatre_Subsequence.py
@@ -1,5 +1,5 @@
-""" [Queston]  Given two non-empty arrays of integers, write a function 
-that determines whether the second array is a subsequence of the first one. """
+""" [Queston]  Given two non-empty arrays of integers, write a function that
+               determines whether the second array is a subsequence of the first one. """
 
 
 
